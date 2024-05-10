766343106f1bb59e37f3a597cd4fb8c1f2fa7905
Duncan Harvey <duncan.harvey@datadoghq.com>
2024-05-10 14:45:11 -0400

    bump datadog node layer version

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "datadog_extension_layer_version" {
 variable "datadog_node_layer_version" {
   description = "Version for the Datadog Node Layer"
   type        = number
-  default     = 108
+  default     = 109
 }
 
 variable "datadog_python_layer_version" {
