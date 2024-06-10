e86d46c4ddf5d17748f322ca72b1df2161483b0d
Duncan Harvey <duncan.harvey@datadoghq.com>
2024-06-10 13:00:06 -0400

    bump extension layer version to 58

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@
 variable "datadog_extension_layer_version" {
   description = "Version for the Datadog Extension Layer"
   type        = number
-  default     = 57
+  default     = 58
 }
 
 variable "datadog_node_layer_version" {
