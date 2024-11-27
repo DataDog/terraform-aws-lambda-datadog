2e3a3169becca6b895ddd35bb36f7b2077a723c5
Aleksandr Pasechnik <aleksandr.pasechnik@datadoghq.com>
2024-11-27 10:10:26 -0500

    chore: [SVLS-5989] add Node22 support

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ module "lambda-datadog" {
   function_name = "example-function"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
-  runtime       = "nodejs20.x"
+  runtime       = "nodejs22.x"
   memory_size   = 256
 
   environment_variables = {
--- examples/node/main.tf
+++ examples/node/main.tf
@@ -55,7 +55,7 @@ module "lambda-datadog" {
   function_name = "terraform-example-node-${var.datadog_service_name}-function"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
-  runtime       = "nodejs20.x"
+  runtime       = "nodejs22.x"
   memory_size   = 256
 
   environment_variables = {
